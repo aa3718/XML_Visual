c88a51f1f6dbc102052e3b9c882e77ba2c38f2e1
aa3718 <aa3718@ic.ac.uk>
2019-08-01 08:42:14 +0100

    Constructed an instance of a Policy.

---
+++ src/allPkg/elements.java
@@ -0,0 +1,15 @@
+package allPkg;
+
+public interface elements {
+
+    void getAsset(Asset ass);
+
+    void getAction(Action act);
+
+    void getContraint(Constraint constraint);
+
+    void getParty(Party party);
+
+    void getDuty(Duty duty);
+
+}
